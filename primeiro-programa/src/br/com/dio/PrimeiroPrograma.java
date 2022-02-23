5e66ba88b2caf6ddc104d5b13be9619fd7d9e781
eadaianne <eadaianne@gmail.com>
2022-02-23 12:05:05 -0300

    aaaaaaaaaaa

---
+++ primeiro-programa/src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,12 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+        /*System.out.println("Hello World!");*/
+    }
+}
